13618f51b8c2c92f9ea5c3c59348e421931f39cf
Dusan <dusan.vlajkovac@outlook.com>
2024-04-10 19:17:43 +0200

    Added a email contact function to a Home page and updated the testimonials.

--- src/main/views.py
+++ src/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Certificates
-
+from django.shortcuts import redirect
 # Create your views here.
 
 def home_page(request):
